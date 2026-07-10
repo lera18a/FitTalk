c2e614a27a956240935f853abdfc479ea3eb2bc1
lera_18a <lera_18a@mail.ru>
2026-07-10 12:07:14 +0300

    feat: added supabase and configs file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:fit_talk/app/app.dart';
+import 'package:fit_talk/core/configs/f_t_configs.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
+  await Supabase.initialize(
+    url: FTConfigs.supabaseUrl,
+    publishableKey: FTConfigs.supabasePublishableKey,
+  );
   runApp(const MyApp());
 }
